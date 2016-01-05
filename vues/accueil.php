b25d7436104fd4021623813605600cdf4e03d53c
Nawhal <nawhal.sayarh@etu.udamail.fr>
2016-01-05 11:07:50 +0100

    Page d'accueil presque finie
    A la mano !!

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr" id="csstyle">
+<html lang="fr">
     <head>
         <?php
         require_once 'header.php';
@@ -9,42 +9,45 @@
         <?php
         require_once 'menu.php';
         ?>
-</div>
-    <div class="section --blue --first">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="illu center-block">
-                        <h1 class="+handwritten text-center">
-                            Doctor Who Fanpage
-                        </h1>
+        <div class="presentation">
+            <div class="centrer">
+                <h1>
+                    Doctor Who Fanpage
+                </h1>
 
-                        <div class="ribbon">
-                            <span>
-                                Pour les fans français de Doctor Who.
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <p class="claim text-center">D&eacute;couvrez les actualit&eacute;s Doctor Who les plus juteuses r&eacute;unies en un site !</p>
-                </div>
+                <p class="presentation_sous-titre">
+                        Pour les fans français de Doctor Who.
+                </p>
+                <p class="presentation_sous-sous-titre">
+                    D&eacute;couvrez les actualit&eacute;s Doctor Who les plus juteuses r&eacute;unies en un site !
+                </p>
             </div>
         </div>
-    </div>
+
+        <div class="section articles">
+            <?php
+            for ($i = 1; $i <= 3; $i++) {
+            ?>
+                <div class="articles_element">
+                    <h3 class="centrer">
+                        Fin de la saison !
+                    </h3>
+                    <p>
+                        L'&eacute;pisode de No&emacr;l a marqu&eacute; la fin de la saison 9. Qui sera 
+                        la/le prochain compagnon? Reverra-t-on River Song? Beaucoup de questions se pose sur la saison 10 !
+                    </p>
+                        <a href="./accueil.php" class="button center-block">En savoir plus</a>
+                    </p>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+<!--
     <div class="section --dark --sm">
         <div class="container">
             <div class="row">
                 <div class="infos">
-                    <?php
-                    for ($i = 1; $i <= 3; $i++) {
-                    ?>
                     <div class="col-md-4 infos__item">
                         <div class="panel">
                             <div class="panel__heading --icon-cal">
@@ -57,14 +60,11 @@
                             </div>
                         </div>
                     </div>
-                    <?php
-                    }
-                    ?>
                 </div>
             </div>
         </div>
     </div>
-    <!-- END RED -->
+-->
 
     <?php
     require_once 'footer.php';
--- vues/footer.php
+++ vues/footer.php
@@ -1,10 +1,5 @@
-<div class="section --solid-dark --no-shadow --sm">
-    <div class="container">
-        <div class="row footer-imprint">
-            <div class="col-lg-push-6 text-right">
-                <span>2016 C&eacute;dric Paris & Nawhal Sayarh</span>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="./frontend.js"></script>
+<footer>
+    <p class="droite section">
+        2016 C&eacute;dric Paris & Nawhal Sayarh
+    </p>
+</footer>
